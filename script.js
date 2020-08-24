c49bcc36b899036506de316afa2fdb0e289f3a28
Nader Frigui <nadfri@gmail.com>
2020-08-25 00:10:45 +0200

    ajout du resize 100% mobile

--- script.js
+++ script.js
@@ -180,6 +180,13 @@ document.getElementById("downBtn").addEventListener("click", function(){
     download(filename, text);
 }, false);
 
+
+/************Permettre le 100vh sur mobile */
+let vh = window.innerHeight * 0.01;
+const hauteur = window.innerHeight;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 //Empeche le resizing à l'apparition du clavier
 const metas = document.getElementsByTagName('meta');
 metas[1].content = 'width=device-width, height=' + window.innerHeight + ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
